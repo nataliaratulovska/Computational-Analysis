eb7be317a8d102b8439701d61a4618275d8dc791
Yannik Herbst <yannik.herbst@uni-wuerzburg.de>
2025-05-20 20:18:07 +0200

    Rework to Grapviz and Recursion

--- nearest-neighbor-viz.py
+++ nearest-neighbor-viz.py
@@ -1,149 +1,186 @@
-import random
-
 import click
-import networkx as nx
-import matplotlib.pyplot as plt
 from gensim.models import fasttext
 from itertools import combinations
 import pickle
+import graphviz as gv
+from uuid import uuid4
+from pydantic import BaseModel, Field, ConfigDict
+from matplotlib import cm
+from matplotlib.colors import rgb2hex
+
+config: 'Config'
+cmap = [rgb2hex(color) for color in cm.plasma((range(256)))]
+negatives = []
+
+class Config(BaseModel):
+    """
+    Configuration class for the FastText model.
+    """
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    model_file: str = Field(default='cc.en.300.bin', description='Model to use. Filename in models/ directory.')
+    positives: list = Field(default=[], description='List of positive words to use.')
+    negatives: list = Field(default=[], description='List of negative words to use.')
+    topn: int = Field(default=10, description='Number of nearest neighbors to find.')
+    subt_topn: int = Field(default=3, description='Graph type to use.')
+    combine: bool = Field(default=False, description='Combine the positives to a conglomerate.')
+    vocab_restriction: int = Field(default=100000, description='Graph type to use.')
+    round_count: int = Field(default=3, description='How much the similarity measure for the labels gets rounded.')
+    depth: int = Field(default=2, description='How deep the graph should be.')
+    model: fasttext.FastText = Field(default=fasttext.FastText, description='Model to use.')
+    single_occurrence: bool = Field(default=True, description='Dont allow multiple occurrences of the same word.')
 
 
 # Dont use matplotlib > 3.9.X
 # https://github.com/RVC-Project/Retrieval-based-Voice-Conversion-WebUI/issues/2411
-#TODO: Add more documentation to the functions eg param descriptions etc
 
-def create_base_word_graph(words, model, combine):
+def add_sub_nodes(graph, word, parent_node_id, hierarchy=1):
     '''
-    Create a graph of the base words.
+    Add sub nodes to the graph.
+    word: Word to use.
+    parent_node_id: Node id to use.
+    hierarchy: Current depth in the graph.
+    visited: Set of already visited words.
     '''
-    graph = nx.Graph()
-    if not combine:
-        for word in words:
-            graph.add_node(word, node_color='#FFAEBC', type='base_word')
-        for combination in combinations(words, 2):
-            similarity = model.wv.similarity(combination[0], combination[1])
-            click.echo(f"Similarity between {combination[0]} and {combination[1]}: {similarity}")
-            click.echo('\n')
-            graph.add_edge(combination[0], combination[1], weight=similarity, color='#FBE7C6', label=similarity, len=(similarity*10)**2)
+    if config is None:
+        raise ValueError("Config is not initialized.")
+    if hierarchy > config.depth:
+        return
+    # Get neighbors
+    neighbors = config.model.wv.most_similar(
+        word,
+        topn=config.subt_topn if hierarchy > 1 else config.topn,
+        restrict_vocab=config.vocab_restriction
+    )
+    for neighbor in neighbors:
+        current_node_id = str(uuid4())
+        graph.node(
+            current_node_id,
+            neighbor[0],
+            fillcolor=cmap[len(cmap) // config.depth * (hierarchy-1)],
+        )
+        graph.edge(
+            parent_node_id,
+            current_node_id,
+            label=str(round(neighbor[1], config.round_count)),
+        )
+        # Recursively add sub-nodes
+        add_sub_nodes(
+            graph,
+            neighbor[0],
+            current_node_id,
+            hierarchy=hierarchy + 1
+        )
+
+
+def create_graph():
+    '''
+    Create a base nodes from userinput.
+    '''
+    # Create a graph, default styles are set here
+    graph = gv.Graph(comment='Graph for: ' + "-".join(config.positives),
+                     graph_attr={
+                        'bgcolor': '#2f4f4f',
+                        'label': 'Graph for: ' + "-".join(config.positives),
+                        'overlap': 'false',
+                     },
+                     node_attr={
+                         'shape': 'circle',
+                         'style': 'filled',
+                         'fontcolor': '#ffffff',
+                         'fontsize': '12',
+                     },
+                     edge_attr={
+                         'color': '#FBE7C6',
+                         'label': 'weight',
+                         'fontsize': '10',
+                         'fontcolor': '#ffffff'
+                     },
+                     engine='neato'
+                     )
+    if not config.combine:
+        node_mapping = []
+        for word in config.positives:
+            node_id = str(uuid4())
+            graph.node(node_id, word, color='#a6335f')
+            add_sub_nodes(graph, word, node_id)
+            node_mapping.append((node_id, word))
+        for combination in combinations(node_mapping, 2):
+            similarity = config.model.wv.similarity(combination[0][1], combination[1][1])
+            click.echo(f"Similarity between {combination[0][1]} and {combination[1][1]}: {similarity}")
+            graph.edge(combination[0][0], combination[1][0], label=str(round(similarity, config.round_count)))
     else:
-        graph.add_node(str(words), node_color='#FFAEBC', type='base_word')
+        node_id = str(uuid4())
+        graph.node(node_id, str(config.positives), color=cmap[0])
     return graph
-
-def create_subgraphs(base_graph, positives, model, topn, subttopn, vocabres, negatives, combine):
-    '''
-    Create subgraphs of the nearest neighbors of the base words.
-    '''
-    #TODO: Naive implementation, should be improved.
-    #TODO: No functionality for negatives. Needs to be added.
-    if not combine:
-        for word in positives:
-            neighbors = model.wv.most_similar(word, topn=topn, restrict_vocab=vocabres)
-            for neighbor in neighbors:
-                base_graph.add_node(neighbor[0], node_color='#FBE7C6', type='sub_word_1')
-                base_graph.add_edge(word, neighbor[0], weight=neighbor[1])
-                sub_neighbors = model.wv.most_similar(neighbor[0], topn=subttopn, restrict_vocab=vocabres,
-                                                      )
-                for sub_neighbor in sub_neighbors:
-                    base_graph.add_node(sub_neighbor[0], node_color='#FBE7C6', type='sub_word_2')
-                    base_graph.add_edge(neighbor[0], sub_neighbor[0], weight=sub_neighbor[1], negative=positives)
-    else:
-        neighbors = model.wv.most_similar(positives, topn=topn, restrict_vocab=vocabres)
-        for neighbor in neighbors:
-            base_graph.add_node(neighbor[0], node_color='#FBE7C6', type='sub_word_1')
-            base_graph.add_edge(str(positives), neighbor[0], weight=neighbor[1])
-            sub_neighbors = model.wv.most_similar(neighbor[0], topn=subttopn, restrict_vocab=vocabres)
-            for sub_neighbor in sub_neighbors:
-                base_graph.add_node(sub_neighbor[0], node_color='#FBE7C6', type='sub_word_2')
-                base_graph.add_edge(neighbor[0], sub_neighbor[0], weight=sub_neighbor[1], negative=positives)
-    return base_graph
 
 
 @click.command()
 @click.option('--positive', prompt='Single or multiple words seperated by a whitespace.'
                                                  'These words are used to find the nearest neighbors. If used with '
                                                  'combine the words act as a conglomerate.', required=True)
-@click.option('--negative', default=None, help='Single or multiple words seperated by a whitespace.'
+@click.option('--negative', default='', help='Single or multiple words seperated by a whitespace.'
                                                  'These words will be excluded from the nearest neighbors.')
 @click.option('--topn', default=10, help='Number of nearest neighbors to find.')
 @click.option('--subttopn', default=3, help='Graph type to use.')
-@click.option('--model', default='cc.en.300.bin', help='Model to use. Filename in models/ directory.')
-@click.option('--combine', is_flag=False, help='Combine the positives to a conglomerate.')
-#TODO: Maybe find a better default for this?
+@click.option('--modelfile', default='cc.en.300.bin', help='Model to use. Filename in models/ directory.')
+@click.option('--combine', is_flag=True, default=False, help='Combine the positives to a conglomerate.')
 @click.option('--vocabres', default=100000, help='Graph type to use.')
-@click.option('--roundcount', default=3, help='How much the similiarity measure for the labels gets rounded.')
-def main(positive, negative, topn, subttopn, model, combine, vocabres, roundcount):
+@click.option('--roundcount', '-r', default=3, help='How much the similiarity measure for the labels gets rounded.')
+@click.option('--depth', '-d', default=2, help='How deep the graph should be.')
+@click.option('--verbose', '-v', is_flag=True, default=False, help='Verbose output.')
+@click.option('--singleoccurrence', '-so', is_flag=True, default=True, help='Dont allow multiple occurrences of the same word.')
+def main(positive, negative, topn, subttopn, modelfile, combine, vocabres, roundcount, depth, verbose, singleoccurrence):
     '''
     Main function to load the pre-trained word vectors and find the most similar words.
     Binary fasttext model has some drawbacks. Checkout https://radimrehurek.com/gensim/models/_fasttext_bin.html
     '''
-    positives = positive.split(" ")
-    negatives = negative.split(" ") if negative else None
+    global config
+    # Set the config
+    config = Config(
+        model_file=modelfile,
+        positives=positive.split(" "),
+        negatives=negative.split(" ") if negative else [],
+        topn=topn,
+        subt_topn=subttopn,
+        combine=combine,
+        vocab_restriction=vocabres,
+        round_count=roundcount,
+        depth=depth,
+        single_occurrence=singleoccurrence
+    )
+    # Print the config
+    if verbose:
+        click.echo('Using model: ' + modelfile + '\n')
+        click.echo('Using positives: ' + positive + '\n')
+        click.echo('Using negatives: ' + negative + '\n')
+        click.echo('Using topn: ' + str(topn) + '\n')
+        click.echo('Using subt_topn: ' + str(subttopn) + '\n')
+        click.echo('Using combine: ' + str(combine) + '\n')
+        click.echo('Using vocabres: ' + str(vocabres) + '\n')
+        click.echo('Using roundcount: ' + str(roundcount) + '\n')
+        click.echo('Using depth: ' + str(depth) + '\n')
+    else:
+        click.echo('Using model: ' + modelfile + '\n')
 
-    click.echo('Using model: ' + model + '\n')
+    # Load the model
     click.echo('Loading model...\n')
-    model = fasttext.load_facebook_model('models/' + model)
+    config.model = fasttext.load_facebook_model('models/' + modelfile)
 
-    click.echo('Creating Basegraph...\n')
-    base_graph = create_base_word_graph(positives, model, combine=combine)
+    # Create the graph
+    click.echo('Creating Graph...\n')
+    graph = create_graph()
+    graph.render(view=True, filename=f'graph-{"-".join(config.positives)}', format='png')
 
-    click.echo('Creating Subgraphs...\n')
-    graph = create_subgraphs(base_graph, positives, model, topn, subttopn, vocabres, negatives, combine=combine)
-
-    pos = nx.nx_pydot.graphviz_layout(graph)
-    fig = plt.figure()
-    nx.draw(graph, pos, with_labels=True, font_size=8)
-    edge_labels = dict([((n1, n2), round(d['weight'], roundcount))
-                        for n1, n2, d in graph.edges(data=True)])
-    print(edge_labels)
-    #color_map = {'base_word': '#a6335f', 'sub_word_1': '#e194bc', 'sub_word_2': '#86aba7'}
-    #node_colors = dict([(n, color_map[typee]) for n, typee in nx.get_node_attributes(base_graph, 'type'))
-    ATTRIBUTE_NAME = 'type'
-    COLOR_SCHEME = {
-        'base_word': '#a6335f',
-        'sub_word_1': '#e194bc',
-        'sub_word_2': '#86aba7'
-    }
-    colors = [COLOR_SCHEME[graph.nodes[node][ATTRIBUTE_NAME]] for node in list(graph.nodes())]
-
-    print([graph.nodes[node][ATTRIBUTE_NAME] for node in list(graph.nodes())])
-    nx.draw_networkx_nodes(graph, pos, node_color=colors, cmap=COLOR_SCHEME.values(), node_size=500, edgecolors='#ffffff'
-                           , alpha=0.9)
-    nx.draw_networkx_edges(graph, pos, edge_color='#86aba7', width=4, alpha=0.8)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels,
-                                 font_color='#ffffff', font_size=8, rotate=False, bbox=dict(alpha=0))
-
-    fig.set_facecolor('#2F4F4F')
-    fig.set_label('Nearest Neighbor Graph for: ' + str(positives))
-    plt.savefig('graph.png')
+    # Uncomment below to create new test graph data
+    # with open('test_graph_data.pickle', 'wb') as f:
+    #     pickle.dump(graph, f)
 
 
 def test_design():
     # For testing without the need to run the whole script
     with open('test_graph_data.pickle', 'rb') as f:
         graph = pickle.load(f)
-    pos = nx.nx_pydot.graphviz_layout(graph)
-    fig = plt.figure()
-    nx.draw(graph, pos, with_labels=True, font_size=8)
-    edge_labels = dict([((n1, n2), round(d['weight'], 3))
-                        for n1, n2, d in graph.edges(data=True)])
-    print(edge_labels)
-    ATTRIBUTE_NAME = 'type'
-    COLOR_SCHEME = {
-        'base_word': '#a6335f',
-        'sub_word_1': '#e194bc',
-        'sub_word_2': '#86aba7'
-    }
-    colors = [COLOR_SCHEME[graph.nodes[node][ATTRIBUTE_NAME]] for node in list(graph.nodes())]
-    print([graph.nodes[node][ATTRIBUTE_NAME] for node in list(graph.nodes())])
-    nx.draw_networkx_nodes(graph, pos, node_color=colors, cmap=COLOR_SCHEME.values(), node_size=500,
-                           edgecolors='#ffffff'
-                           , alpha=0.9)
-    nx.draw_networkx_edges(graph, pos, edge_color='#86aba7', width=4, alpha=0.8)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels,
-                                 font_color='#ffffff', font_size=8, rotate=False, bbox=dict(alpha=0))
-
-    fig.set_facecolor('#2F4F4F')
-    plt.savefig('test_graph.png')
+    graph.render(view=True)
 
 
 #TODO: Add your names and rearrange them alphabetically
@@ -154,5 +191,4 @@ if __name__ == "__main__":
           "-----------------------------------\n")
 
     main()
-    #test_design()
 
